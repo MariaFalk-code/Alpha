1fc8aaec77181cf1af182b374ae859e525c40bbb
Maria Falk <mariacelf@gmail.com>
2025-05-02 13:26:18 +0200

    Added _ProjectCard with design, called Admin/Index with some hardcoded values for now.

---
+++ WebApp/Models/ProjectCardModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class ProjectCardModel
+    {
+        public string ProjectName { get; set; }
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+        public string Deadline { get; set; }
+        public List<string> TeamMembers { get; set; } = new();
+    }
+}
