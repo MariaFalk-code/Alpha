fa43a661691ec5215c2ada6b6849baa83efddb72
Maria Falk <mariacelf@gmail.com>
2025-05-04 01:31:52 +0200

    Added a validation script and injected the script partial into the partials containing forms.

---
+++ WebApp/wwwroot/js/validate.js
@@ -0,0 +1,44 @@
+﻿const validateInput = (input) => {
+    let errorSpan = document.querySelector(`span[data-valmsg-for="${input.name}"]`);
+    if (!errorSpan) return;
+
+    let errorMessage = "";
+    let value = input.value.trim();
+
+    if (input.hasAttribute("data-val-required") && value=== "") {
+        errorMessage = input.getAttribute("data-val-required");
+    }
+    if (input.hasAttribute("data-val-regex") && value !== "") {
+        let pattern = new RegExp(input.getAttribute("data-val-regex-pattern"));
+        if (!pattern.test(value)) {
+            errorMessage = input.getAttribute("data-val-regex");
+        }
+    }
+    if (errorMessage) {
+        input.classList.add("input-validation-error");
+        errorSpan.classList.remove("field-validation-valid")
+        errorSpan.classList.add("field-validation-error");
+        errorSpan.textContent = errorMessage;
+    }
+    else {
+        input.classList.remove("input-validation-error");
+        errorSpan.classList.remove("field-validation-error");
+        errorSpan.classList.add("field-validation-valid");
+        errorSpan.textContent = "";
+    }
+    
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    if (!form) return;
+
+    const inputs = form.querySelectorAll("input[data-val='true']");
+
+    inputs.forEach(input => {
+        input.addEventListener("input", () => {
+            validateInput(input);
+        })
+    })
+});
