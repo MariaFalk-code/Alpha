e571b6ca9c03c841b7116ec70fe35b4aff828d4b
Maria Falk <mariacelf@gmail.com>
2025-05-04 02:00:19 +0200

    Added attributes for client side validation to project-related forms.

--- WebApp/Models/ViewModels/ProjectFormModel.cs
+++ WebApp/Models/ViewModels/ProjectFormModel.cs
@@ -1,14 +1,28 @@
-﻿namespace WebApp.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.ViewModels
 {
     public class ProjectFormModel
     {
+        [Required(ErrorMessage = "Project name is required.")]
         public string ProjectName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Client name is required.")]
         public string ClientName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; } = null!;
 
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Start date is required.")]
         public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "End date is required.")]
         public DateTime EndDate { get; set; }
 
+        [Required(ErrorMessage = "An estimated budget is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Budget must be greater than 0.")]
         public decimal Budget { get; set; }
 
         public List<Guid> SelectedTeamMemberIds { get; set; } = new();
